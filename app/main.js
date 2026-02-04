9456b9bb7d15daf88670b5c39a79369d1b89e34e
Leo <leandroprofka@icloud.com>
2026-02-04 09:45:11 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -8,9 +8,16 @@ const rl = readline.createInterface({
 // Uncomment this block to pass the first stage
 const prompt = () => {
   rl.question("$ ", (answer) => {
+    if (answer === "exit") {
+      rl.close();
+      return;
+    }
     console.log(`${answer}: command not found`);
     prompt();
   });
 };
 
+rl.on("close", () => {
+  process.exit(0);
+});
 prompt();
